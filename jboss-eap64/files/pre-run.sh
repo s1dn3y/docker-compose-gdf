12b52d82a1ba8a51f234d70cdda4346337e985b5
Sidney Amaral <sidney@teclatab.com.br>
2017-08-16 17:30:59 -0300

    bind do Admin Console em 0.0.0.0

--- jboss-eap64/files/pre-run.sh
+++ jboss-eap64/files/pre-run.sh
@@ -12,7 +12,7 @@ if [ "$DEV" = true ]; then
 	echo **** \}
 	# binds to all interfaces on debug mode
 	export DEBUG=true
-	. /opt/eap/bin/standalone.sh -b 0.0.0.0
+	. /opt/eap/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 else
 #	. /opt/eap/bin/openshift-launch.sh
 	. /opt/eap/bin/standalone.sh
--- jboss-eap70/files/pre-run.sh
+++ jboss-eap70/files/pre-run.sh
@@ -9,7 +9,7 @@ if [ "$DEV" = true ]; then
 	echo **** UID: $UID
 	# binds to all interfaces on debug mode
 	export DEBUG=true
-	. /opt/eap/bin/standalone.sh -b 0.0.0.0
+	. /opt/eap/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 else
 	. /opt/eap/bin/openshift-launch.sh
 fi
