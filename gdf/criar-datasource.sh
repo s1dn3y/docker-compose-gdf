10055025326be4e766c0926b377d67e8eaf10b0e
Sidney Amaral <sidney@teclatab.com.br>
2017-08-21 16:37:49 -0300

    ajustes para criação de datasource por script e pelo maven

---
+++ gdf/criar-datasource.sh
@@ -0,0 +1,1 @@
+java -jar jboss-cli-client.jar --controller=127.0.0.1:19990 --user=admin --password=@dmin123456 --connect --file=criar-datasource.commands
--- gdf/jboss-eap70/files/jboss-cli/criar-datasource.sh
+++
@@ -1,1 +0,0 @@
-java -jar jboss-cli-client.jar --controller=127.0.0.1:19990 --user=admin --password=@dmin123456 --connect --file=commands.txt
