a8fb34c9a8d7437340175836d7c80d678c78d7d9
Sidney Amaral <sidneyamaral@rio.rj.gov.br>
2017-09-19 11:58:32 -0300

    ajustes

--- gdf/jboss-eap64/files/pre-run.sh
+++ gdf/jboss-eap64/files/pre-run.sh
@@ -7,13 +7,9 @@ rm -rf /tmp/deployments
 if [ "$DEV" = true ]; then
 	echo ** DEV MODE ON
 	echo **** UID: $UID
-	echo **** Files on root dir: / \{
-	ls -lha /
-	echo **** \}
 	# binds to all interfaces on debug mode
 	export DEBUG=true
 	. /opt/eap/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 else
-#	. /opt/eap/bin/openshift-launch.sh
-	. /opt/eap/bin/standalone.sh
+	. /opt/eap/bin/openshift-launch.sh
 fi
